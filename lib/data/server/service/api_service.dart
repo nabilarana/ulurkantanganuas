be0fcc2e9766ee52c9905aed20ba77a83a6cc820
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-12 23:27:39 +0700

    menambahkan fungsi POST request pada ApiService

--- lib/data/server/service/api_service.dart
+++ lib/data/server/service/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   static const String baseUrl = 'http://192.168.18.169:8000/api';
 
   static const Duration timeout = Duration(seconds: 30);
-  
+
   Future<http.Response> get(String endpoint) async {
     final url = Uri.parse('$baseUrl/$endpoint');
 
@@ -23,6 +23,27 @@ class ApiService {
       return response;
     } catch (e) {
       throw Exception('Error GET request: $e');
+    }
+  }
+
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+
+    try {
+      final response = await http
+          .post(
+            url,
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+            },
+            body: jsonEncode(body),
+          )
+          .timeout(timeout);
+
+      return response;
+    } catch (e) {
+      throw Exception('Error POST request: $e');
     }
   }
 }
