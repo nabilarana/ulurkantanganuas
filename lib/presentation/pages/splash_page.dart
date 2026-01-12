eaa6cd4dad62a61819bb3cafacfd4ccb5ae61377
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-12 22:08:06 +0700

    menambahkan halaman splash dan delay (2s) navigasi

---
+++ lib/presentation/pages/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  Future<void> _navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!mounted) return;
+
+    print('Navigating to login...');
+  }
+}
