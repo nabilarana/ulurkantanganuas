f8437a2f50e8435a76bd952af14440f703b4a764
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-14 14:11:03 +0700

    menambahkan SessionManager untuk pengelolaan sesi pengguna

---
+++ lib/data/server/service/session_manager.dart
@@ -0,0 +1,8 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SessionManager {
+  static const String _keyUserId = 'user_id';
+  static const String _keyUserName = 'user_name';
+  static const String _keyUserEmail = 'user_email';
+  static const String _keyIsLoggedIn = 'is_logged_in';
+}
