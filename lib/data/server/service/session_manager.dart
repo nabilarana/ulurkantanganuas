1e9e2b52243777a9db3e98447318b81b9bcfe162
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-14 14:32:53 +0700

    menambahkan fungsi manajemen sesi login pada SessionManager

--- lib/data/server/service/session_manager.dart
+++ lib/data/server/service/session_manager.dart
@@ -5,4 +5,38 @@ class SessionManager {
   static const String _keyUserName = 'user_name';
   static const String _keyUserEmail = 'user_email';
   static const String _keyIsLoggedIn = 'is_logged_in';
+
+  static Future<void> saveSession(dynamic userData, String? token) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt(_keyUserId, userData.id ?? 0);
+    await prefs.setString(_keyUserName, userData.nama ?? '');
+    await prefs.setString(_keyUserEmail, userData.email ?? '');
+    await prefs.setBool(_keyIsLoggedIn, true);
+  }
+
+  static Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_keyIsLoggedIn) ?? false;
+  }
+
+  static Future<int?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_keyUserId);
+  }
+
+  static Future<String?> getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUserName);
+  }
+
+  static Future<String?> getUserEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUserEmail);
+  }
+
+  static Future<void> clearSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
 }
