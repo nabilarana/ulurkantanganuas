d9db7f086f9e6887556dfdba52b47d805269ef94
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-16 13:50:35 +0700

    update model campaign

--- lib/data/server/model/campaign.dart
+++ lib/data/server/model/campaign.dart
@@ -1,22 +1,22 @@
 import 'dart:convert';
 
 class Campaign {
-  int id;
-  String judul;
-  String deskripsi;
-  String? fotoUtama;
-  double targetDana;
-  double danaTerkumpul;
-  String? categoryName;
-  DateTime createdAt;
+  final int id;
+  final String judul;
+  final String deskripsi;
+  final double targetDana;
+  final double danaTerkumpul;
+  final String? fotoUtama;
+  final String? categoryName;
+  final DateTime createdAt;
 
   Campaign({
     required this.id,
     required this.judul,
     required this.deskripsi,
-    this.fotoUtama,
     required this.targetDana,
     required this.danaTerkumpul,
+    this.fotoUtama,
     this.categoryName,
     required this.createdAt,
   });
@@ -26,26 +26,26 @@ class Campaign {
   String toJson() => json.encode(toMap());
 
   factory Campaign.fromMap(Map<String, dynamic> json) => Campaign(
-        id: json["id"],
-        judul: json["judul"],
-        deskripsi: json["deskripsi"],
-        fotoUtama: json["foto_utama"],
-        targetDana: (json["target_dana"] as num).toDouble(),
-        danaTerkumpul: (json["dana_terkumpul"] as num).toDouble(),
-        categoryName: json["category_name"],
-        createdAt: DateTime.parse(json["created_at"]),
-      );
+    id: json["id"],
+    judul: json["judul"],
+    deskripsi: json["deskripsi"],
+    fotoUtama: json["foto_utama"],
+    targetDana: (json["target_dana"] as num).toDouble(),
+    danaTerkumpul: (json["dana_terkumpul"] as num).toDouble(),
+    categoryName: json["category_name"],
+    createdAt: DateTime.parse(json["created_at"]),
+  );
 
   Map<String, dynamic> toMap() => {
-        "id": id,
-        "judul": judul,
-        "deskripsi": deskripsi,
-        "foto_utama": fotoUtama,
-        "target_dana": targetDana,
-        "dana_terkumpul": danaTerkumpul,
-        "category_name": categoryName,
-        "created_at": createdAt.toIso8601String(),
-      };
+    "id": id,
+    "judul": judul,
+    "deskripsi": deskripsi,
+    "foto_utama": fotoUtama,
+    "target_dana": targetDana,
+    "dana_terkumpul": danaTerkumpul,
+    "category_name": categoryName,
+    "created_at": createdAt.toIso8601String(),
+  };
 
   double get progressPercentage {
     if (targetDana == 0) return 0;
