421418743b3f8e83c30d5a17485823126489b14f
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-16 14:44:12 +0700

    update

--- lib/data/server/service/api_service.dart
+++ lib/data/server/service/api_service.dart
@@ -1,90 +1,98 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.18.169:8000/api';
-
-  static const Duration timeout = Duration(seconds: 30);
+  final String baseUrl = 'http://192.168.18.169:8000/api';
 
   Future<http.Response> get(String endpoint) async {
     final url = Uri.parse('$baseUrl/$endpoint');
-
-    try {
-      final response = await http
-          .get(
-            url,
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            },
-          )
-          .timeout(timeout);
-
-      return response;
-    } catch (e) {
-      throw Exception('Error GET request: $e');
-    }
+    final response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+    log('GET Response: ${response.body}');
+    return response;
   }
 
   Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
     final url = Uri.parse('$baseUrl/$endpoint');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+    log('POST Response: ${response.body}');
+    return response;
+  }
 
+  Future<http.Response> postWithFile(
+    String endPoint,
+    Map<String, String> fields,
+    File? file,
+    String fileFieldName,
+  ) async {
     try {
-      final response = await http
-          .post(
-            url,
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            },
-            body: jsonEncode(body),
-          )
-          .timeout(timeout);
+      final url = Uri.parse('$baseUrl/$endPoint');
+      final request = http.MultipartRequest('POST', url);
 
+      request.fields.addAll(fields);
+
+      if (file != null) {
+        final imageFile = await http.MultipartFile.fromPath(
+          fileFieldName,
+          file.path,
+        );
+        request.files.add(imageFile);
+        log('File added: ${file.path}');
+      }
+
+      log('POST with File to: $url');
+      log('Fields: ${request.fields}');
+      log('Files: ${request.files.length}');
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      log('POST with File Response: ${response.statusCode} - ${response.body}');
       return response;
     } catch (e) {
-      throw Exception('Error POST request: $e');
+      log('Error in postWithFile: $e');
+      rethrow;
     }
   }
 
   Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
     final url = Uri.parse('$baseUrl/$endpoint');
-
-    try {
-      final response = await http
-          .put(
-            url,
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            },
-            body: jsonEncode(body),
-          )
-          .timeout(timeout);
-
-      return response;
-    } catch (e) {
-      throw Exception('Error PUT request: $e');
-    }
+    final response = await http.put(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+    log('PUT Response: ${response.body}');
+    return response;
   }
 
   Future<http.Response> delete(String endpoint) async {
     final url = Uri.parse('$baseUrl/$endpoint');
-
-    try {
-      final response = await http
-          .delete(
-            url,
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            },
-          )
-          .timeout(timeout);
-
-      return response;
-    } catch (e) {
-      throw Exception('Error DELETE request: $e');
-    }
+    final response = await http.delete(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+    log('DELETE Response: ${response.body}');
+    return response;
   }
 }
