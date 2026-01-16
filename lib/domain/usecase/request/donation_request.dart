c67d7034b227eca0c32d9c93b7afb31a46a72f41
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-16 15:04:14 +0700

    update, mengubah method toJson ke toMap

--- lib/domain/usecase/request/donation_request.dart
+++ lib/domain/usecase/request/donation_request.dart
@@ -1,25 +1,30 @@
+import 'dart:io';
+
 class DonationRequest {
-  final int idUser;
-  final int idKampanye;
-  final double jumlah;
-  final String? pesan;
-  final bool anonim;
+  final int campaignId;
+  final double amount;
+  final String donorName;
+  final String? donorEmail;
+  final String? message;
+  final bool isAnonymous;
 
   DonationRequest({
-    required this.idUser,
-    required this.idKampanye,
-    required this.jumlah,
-    this.pesan,
-    this.anonim = false,
+    required this.campaignId,
+    required this.amount,
+    required this.donorName,
+    this.donorEmail,
+    this.message,
+    required this.isAnonymous,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id_user': idUser,
-      'id_kampanye': idKampanye,
-      'jumlah': jumlah,
-      'pesan': pesan,
-      'anonim': anonim ? 1 : 0,
+  Map<String, String> toMap() {
+    return <String, String>{
+      'campaign_id': campaignId.toString(),
+      'amount': amount.toString(),
+      'donor_name': donorName,
+      'donor_email': donorEmail ?? '',
+      'message': message ?? '',
+      'is_anonymous': isAnonymous ? '1' : '0',
     };
   }
 }
