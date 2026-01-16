1a813ede5ad8f30031d9adc7582c2f7d9dc55856
Nabila Rana Pradipta <nabilaranapradipta20@gmail.com>
2026-01-16 21:05:10 +0700

    update, simpan token login dan arahkan user ke homePage atau loginPage otomatis

--- lib/data/server/service/session_manager.dart
+++ lib/data/server/service/session_manager.dart
@@ -4,20 +4,25 @@ class SessionManager {
   static const String _keyUserId = 'user_id';
   static const String _keyUserName = 'user_name';
   static const String _keyUserEmail = 'user_email';
-  static const String _keyIsLoggedIn = 'is_logged_in';
+  static const _keyToken = 'token';
 
-  static Future<void> saveSession(dynamic userData, String? token) async {
+  static Future<void> saveSession(dynamic user, String token) async {
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.setInt(_keyUserId, userData.id ?? 0);
-    await prefs.setString(_keyUserName, userData.nama ?? '');
-    await prefs.setString(_keyUserEmail, userData.email ?? '');
-    await prefs.setBool(_keyIsLoggedIn, true);
+    await prefs.setInt(_keyUserId, user.id);
+    await prefs.setString(_keyUserName, user.nama);
+    await prefs.setString(_keyUserEmail, user.email);
+    await prefs.setString(_keyToken, token);
   }
 
   static Future<bool> isLoggedIn() async {
+    final token = await getToken();
+    return token != null && token.isNotEmpty;
+  }
+
+  static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_keyIsLoggedIn) ?? false;
+    return prefs.getString(_keyToken);
   }
 
   static Future<int?> getUserId() async {
